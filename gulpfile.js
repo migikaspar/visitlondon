eb5d437ff77ca27733c5b2d981b4839c35a7b204
Misa Kaspar <misakaspar@icloud.com>
2017-09-12 00:27:37 +0200

    added gulp task copy

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 
 var SOURCEPATHS = {
-  sassSource : 'src/scss/*.scss'
+  sassSource : 'src/scss/*.scss',
+  htmlSource : 'src/*.html'
 }
 var APPPATH = {
   root : 'app/',
@@ -21,6 +22,11 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(APPPATH.css));
 });
 
+gulp.task('copy', function() {
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATH.root))
+});
+
 gulp.task('serve', ['sass'], function() {
   browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
     server: {
@@ -29,8 +35,9 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+  gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 });
 
 gulp.task('default', ['watch']);
