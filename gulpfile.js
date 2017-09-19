af57004f99e686d4ee69ab13b47f1f8fb07f2800
Misa Kaspar <misakaspar@icloud.com>
2017-09-19 07:54:38 +0200

    move js files

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,13 @@ var clean = require('gulp-clean');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
-  htmlSource : 'src/*.html'
+  htmlSource : 'src/*.html',
+  jsSource : 'src/js/*.js'
 }
 var APPPATH = {
   root : 'app/',
   css : 'app/css',
-  ja : 'app/js'
+  js : 'app/js'
 }
 
 gulp.task('clean-html', function() {
@@ -20,12 +21,21 @@ gulp.task('clean-html', function() {
     .pipe(clean());
 });
 
+gulp.task('clean-scripts', function() {
+  return gulp.src(APPPATH.js + '/*.js', {read: false, force: true})
+    .pipe(clean());
+});
 
 gulp.task('sass', function() {
   return gulp.src(SOURCEPATHS.sassSource)
     .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest(APPPATH.css));
+});
+
+gulp.task('scripts', ['clean-scripts'], function() {
+  gulp.src(SOURCEPATHS.jsSource)
+    .pipe(gulp.dest(APPPATH.js));
 });
 
 gulp.task('copy', ['clean-html'], function() {
@@ -41,9 +51,10 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html'], function() {
+gulp.task('watch', ['serve', 'sass', 'copy', 'clean-html','clean-scripts', 'scripts'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
+  gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
 });
 
 gulp.task('default', ['watch']);
